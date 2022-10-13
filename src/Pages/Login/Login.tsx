ffef5cf01e2fa3ba46fbf6107adc746e91bd564b
tobbyano1234 <adegbesan86@gmail.com>
2022-10-14 00:22:25 +0100

    initial commit

---
+++ src/Pages/Login/Login.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="container">
+      <div className="logo-container">
+        <div className="logo-icon"></div>
+        <div className="image"></div>
+      </div>
+      <div className="login-container">
+        <div>
+          <form>
+            <input type="text" />
+          </form>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
