b285f9bfaeb03b4da5952e2dbd30b39b3a8423d1
tobbyano1234 <adegbesan86@gmail.com>
2022-10-14 11:42:19 +0100

    add login page features

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,19 +1,79 @@
-import React from "react";
+import { useState } from "react";
+import Input from "../../components/Common/Input";
+import logo from "../../Assets/images/logo.svg";
+import loginImage from "../../Assets/images/login-image.svg";
+import "../../Styles/Login.scss";
+import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
-const Login = () => {
+const Login = (): JSX.Element => {
+  const [formData, setFormData] = useState({});
+  const [toggle, setToggle] = useState(true);
+  const navigate = useNavigate();
+
+  const handleChange = (e: any): void => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleClick = (e: any) => {
+    e.preventDefault();
+    navigate("/dashboard");
+  };
+
   return (
-    <div className="container">
-      <div className="logo-container">
-        <div className="logo-icon"></div>
-        <div className="image"></div>
+    <div className="login_container">
+      <Helmet>
+        <title>Lendsqr | Login</title>
+      </Helmet>
+      <div className="container_logo">
+        <div className="container_logo--icon">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="container_logo--image">
+          <img src={loginImage} alt="image" />
+        </div>
       </div>
-      <div className="login-container">
-        <div>
-          <form>
-            <input type="text" />
+      <div className="loginContainer">
+        <div className="loginContainer_info">
+          <h2 className="loginContainer_info--title">Welcome!</h2>
+          <p className="loginContainer_info--text">Enter details to login.</p>
+        </div>
+        <div className="loginContainer_formInput">
+          <form onSubmit={handleClick}>
+            <Input
+              placeholder={"Email"}
+              type={"email"}
+              name={"email"}
+              onChange={handleChange}
+              label={""}
+              labelName={""}
+              className={"loginContainer_formInput--email"}
+              value={undefined}
+            />
+            <div className="loginContainer_formInput--passwordDiv">
+              <input
+                placeholder={"Password"}
+                type={toggle ? "password" : "text"}
+                name={"password"}
+                onChange={handleChange}
+                className="loginContainer_formInput--passwordDiv-password"
+              />
+              <p
+                className="loginContainer_formInput--passwordtext"
+                onClick={() => setToggle(!toggle)}
+              >
+                SHOW
+              </p>
+            </div>
+            <p className="loginContainer_formInput--forgotpassword">
+              FORGOT PASSWORD
+            </p>
+            <button type="submit" className="loginContainer_formInput--btn">
+              LOG IN
+            </button>
           </form>
         </div>
-        <div></div>
       </div>
     </div>
   );
--- src/components/Common/Input.tsx
+++ src/components/Common/Input.tsx
@@ -1,1 +1,29 @@
 import React from "react";
+import "../../Styles/Input.scss";
+
+interface Icontent {
+  type: string;
+  label: string;
+  labelName: string;
+  placeholder: string;
+  name: string;
+  className: string;
+  onChange?: (e?: React.ChangeEvent<HTMLInputElement>) => void;
+  value: undefined | string;
+}
+
+const Input = ({ type, placeholder, name, onChange }: Icontent) => {
+  return (
+    <div className="inputGroup">
+      <input
+        type={type}
+        placeholder={placeholder}
+        name={name}
+        className={"inputGroup_input inputGroup_input--field"}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export default Input;
